c5f348bd20b81b5e1b44017856ba268fe5f43060
IrudiniHasara <57498300+IrudiniHasara@users.noreply.github.com>
2020-03-18 03:36:16 +0530

    Add files via upload

---
+++ TurtlePro.py
@@ -0,0 +1,18 @@
+#draw a letter using turtle
+import turtle
+
+Draw = turtle.Turtle()
+
+""""Name = input("Enter a letter")
+
+if(Name == A):"""""
+Draw.speed(1)
+Draw.left(60)
+Draw.forward(100)
+Draw.right(120)
+Draw.forward(100)
+Draw.back(50)
+Draw.right(120)
+Draw.forward(60)
+
+turtle.done()
